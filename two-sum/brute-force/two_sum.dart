0f0c7f01a5d675b84fc64b4033ee4ee8f3b37887
theDashpuntsag11 <thedashpuntsag@gmail.com>
2022-09-26 11:24:37 +0800

    two_sum_commit

---
+++ two-sum/brute-force/two_sum.dart
@@ -0,0 +1,11 @@
+class Solution {
+  List<int> twoSum(List<int> nums, int target) {
+      for(var i = 0; i < nums.length - 1; i++) {
+          for (var j = i + 1; j < nums.length; j++) {
+              if (nums[i] + nums[j] == target)
+                  return [i, j];
+          }
+      }
+      return [];
+  }
+}
